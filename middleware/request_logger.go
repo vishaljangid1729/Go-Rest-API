73ffddc0a4afb15a3e447c6eeedf04792b4d5369
Vishal Jangid <jangidvishal1999@gmail.com>
2022-07-30 01:39:40 +0530

    reqeust logger added.

---
+++ middleware/request_logger.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"go.uber.org/zap"
+)
+
+func RequestLogger() gin.HandlerFunc {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		stop := time.Since(start)
+
+		uuid := c.GetString("uuid")
+		clientIP := c.ClientIP()
+		clientUserAgent := c.Request.UserAgent()
+		clientReferer := c.Request.Referer()
+		requestBody := c.Request.Body
+		requestPath := c.Request.URL.Path
+
+		statusCode := c.Writer.Status()
+		dataLength := c.Writer.Size()
+
+		if dataLength < 0 {
+			dataLength = 0
+		}
+		log.Info(c.Request.Context(), "Request",
+			zap.String("uuid", uuid),
+			zap.String("method", c.Request.Method),
+			zap.String("path", requestPath),
+			zap.String("timestamp", start.Format(time.RFC1123)),
+			zap.Int("statusCode", statusCode),
+			zap.String("hostname", hostname),
+			zap.String("latency", stop.String()),
+			zap.String("clientIP", clientIP),
+			zap.String("referer", clientReferer),
+			zap.Int("dataLength", dataLength),
+			zap.String("userAgent", clientUserAgent),
+			zap.Any("request", requestBody),
+		)
+	}
+}
