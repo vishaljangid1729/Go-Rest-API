47762cf9c80db478f57c4ee28aa825ecb26caa99
Vishal Jangid <jangidvishal1999@gmail.com>
2022-07-30 01:38:00 +0530

    main.go gin server added.

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"rest-api/env"
+	"rest-api/routes"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	env := env.GetEnv()
+
+	apiPort := env["API_PORT"]
+
+	gin.SetMode(gin.ReleaseMode)
+	gin.DisableConsoleColor()
+
+	server := &http.Server{
+		Addr:         ":" + apiPort,
+		Handler:      routes.GetRoutes(),
+		ReadTimeout:  time.Second * 2,
+		WriteTimeout: time.Second * 2,
+	}
+
+	fmt.Println("Starting server on port: " + apiPort)
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println(err)
+	}
 
 }
